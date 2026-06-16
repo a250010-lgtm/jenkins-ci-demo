39810620b3595c62f97b204569b0d64658eae6bd
VaishnaviGavali <a250010@famt.ac.in>
2026-06-16 04:14:40 -0700

    trigger jenkins build

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI Pipeline");
+       System.out.println("Updated build trigger test");
     }
 }
